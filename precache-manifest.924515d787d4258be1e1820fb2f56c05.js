d66c9e810177b11dbd015ce5328e9b856b027726
Abdul Wahab <abdulwahab@Abduls-MacBook-Pro-311.local>
2020-04-15 19:09:06 +0200

    Updates

---
+++ precache-manifest.924515d787d4258be1e1820fb2f56c05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422a2305255379cc7a57855b53da8bba",
+    "url": "/advice-app-react/index.html"
+  },
+  {
+    "revision": "2b8016558bccf99db241",
+    "url": "/advice-app-react/static/css/main.d709c4a6.chunk.css"
+  },
+  {
+    "revision": "54b29259f3dda9b48aeb",
+    "url": "/advice-app-react/static/js/2.d3e74876.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/advice-app-react/static/js/2.d3e74876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8016558bccf99db241",
+    "url": "/advice-app-react/static/js/main.dbd7c8b7.chunk.js"
+  },
+  {
+    "revision": "cf9abaa9b0e856118b2b",
+    "url": "/advice-app-react/static/js/runtime-main.de579219.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/advice-app-react/static/media/city.91c109fc.jpg"
+  }
+]);
